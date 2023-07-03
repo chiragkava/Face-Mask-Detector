77c8e8214d6c6d3d8f65df8897867cbb48adee04
Chirag Kava <61086004+chiragkava@users.noreply.github.com>
2023-07-03 09:49:52 +0530

    refactoring

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 import io
-import os
-from PIL import Image,ImageOps
-import torch
 from pathlib import Path
-from models.yolo import Model, attempt_load
+
+import torch
+import constants
+from PIL import Image, ImageOps
+from flask import Flask, request, redirect, render_template, url_for, flash
+
+from models.yolo import attempt_load
 from utils.general import set_logging
 from utils.torch_utils import select_device
-from flask import Flask,request, redirect,render_template, url_for,flash
-
 
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
@@ -17,7 +18,8 @@ app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 def home():
     return render_template('index.html')
 
-@app.route("/", methods=["GET", "POST"])
+
+@app.route("/", methods=["POST"])
 def predict():
     if request.method == "POST":
         if "file" not in request.files:
@@ -27,46 +29,57 @@ def predict():
             flash('Please select a file')
             return redirect('/')
 
+        # validate if its image or not
+        if file.content_type not in constants.ALLOWED_IMAGE_TYPE:
+            flash("Image format not supported. Supported formats: {}".format(constants.ALLOWED_IMAGE_TYPE))
+            return redirect('/')
+
         img_bytes = file.read()
         img = Image.open(io.BytesIO(img_bytes))
-        new_img = ImageOps.exif_transpose(img)
-        model = custom(path='last.pt')
-        model.conf = 0.4  
-        model.iou = 0.3 
-        results = model(new_img, size=480)
-        filename="image0.jpg"
-        print("inside predict:",filename)
-        results.save("static/saved")  
-        return render_template("index.html",filename=filename)
+        transposed_image = ImageOps.exif_transpose(img)
 
-@app.route('/display/<filename>')
+        # load model
+        model = get_custom_model()
+
+        # set model params
+        model.conf = 0.4
+        model.iou = 0.3
+
+        # compute and save result
+        results = model(transposed_image, size=480)
+        results.save(constants.SAVE_DIR)
+
+        return render_template("index.html", filename=constants.DEFAULT_FILE_NAME)
+
+
+@app.route('/display/<filename>', methods=["GET"])
 def display_image(filename):
-    return redirect(url_for('static', filename="saved/"+filename))
+    return redirect(url_for('static', filename="saved/" + filename))
 
-@app.route("/try/<name>")
-def try_it(name):
-    filename = name
-    print("inside try: ",filename)
-    model = custom(path="last.pt")
-    img = Image.open("static/sample/"+name)
-    results = model(img,size=640)
-    print("inside try")
-    results.save("static/saved")
-    return render_template("index.html",filename=filename)
 
-def _create(name, pretrained=True, channels=3, classes=80, autoshape=True, verbose=True, device=None):
+@app.route("/try/<file_name>", methods=["GET"])
+def try_it(file_name):
+    # load model
+    model = get_custom_model()
+
+    img = Image.open(constants.IMAGE_BASE_PATH + file_name)
+
+    results = model(img, size=640)
+    results.save(constants.SAVE_DIR)
+
+    return render_template("index.html", filename=file_name)
+
+
+def get_custom_model(autoshape=True, verbose=True, device=None):
+    # enable logging
     set_logging(verbose=verbose)
 
-    fname = Path(name).with_suffix('.pt') 
-    model = attempt_load(fname, map_location=torch.device('cpu')) 
+    model_path = Path(constants.MODEL_PATH)
+    model = attempt_load(model_path, map_location=torch.device('cpu'))
     if autoshape:
-        model = model.autoshape() 
+        model = model.autoshape()
     device = select_device('0' if torch.cuda.is_available() else 'cpu') if device is None else torch.device(device)
     return model.to(device)
-    
-    
-def custom(path='path/to/model.pt', autoshape=True, verbose=True, device=None):
-    return _create(path, autoshape=autoshape, verbose=verbose, device=device)
 
 
 if __name__ == "__main__":
